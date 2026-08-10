108bf112bb94eadb8e5e02a70815f021d7bca1c0
Pushpendra Shah <pushpendra7725@gmail.com>
2026-08-11 00:38:37 +0530

    initialize HashTable

--- Companies Specific/Day04_LongestSubarrayWithSumK.java
+++ Companies Specific/Day04_LongestSubarrayWithSumK.java
@@ -3,6 +3,7 @@ class Day04_LongestSubarrayWithSumK {
     public static int longestSubarrayWithSumK(int[] nums, int k) {
         int count = 0, prefix = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
         for(int num : nums) {
             prefix += num;
             if(map.containsKey(prefix-k)) {
