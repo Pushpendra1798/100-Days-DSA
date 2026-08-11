48e158b6e7db4bfb04d14f5bbd60fc704d6f2395
Pushpendra Shah <pushpendra7725@gmail.com>
2026-08-11 12:33:01 +0530

    Optimize using hashmap

--- Companies Specific/Day06_MajorityElement.java
+++ Companies Specific/Day06_MajorityElement.java
@@ -1,7 +1,8 @@
-import java.util.Scanner;
+import java.util.*;
 public class Day06_MajorityElement {
+
+    // Using Nested Loop ------------------------->
     public static int mejorityElement(int[] nums) {
-        
         int n = nums.length;
         for(int i=0; i<n; i++) {
             int count = 0;
@@ -17,6 +18,25 @@ public class Day06_MajorityElement {
         return -1;
     }
 
+
+    // Optimize approach using HashMap ------------------->
+    public static int mejorityElement1(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums) {
+            map.put(num, map.getOrDefault(num, 0)+1); // count frequency
+        }
+
+        int result = 0;
+        for(int key : map.keySet()) {  // Traverse on keySet
+            if(map.get(key) > n/2) {
+                result = key; // mejority element
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter no. of elements: ");
@@ -26,7 +46,7 @@ public class Day06_MajorityElement {
             for(int i=0; i<N; i++) {
                 nums[i] = sc.nextInt();
             }
-            System.out.println("Mejority element: " + mejorityElement(nums));
+            System.out.println("Mejority element: " + mejorityElement1(nums));
         }
     }
 }
