e31d856c1c23b9b8c64cce210a622cbff5e4be90
Pushpendra Shah <pushpendra7725@gmail.com>
2026-08-14 18:06:04 +0530

    BruteForce- Linear Search

--- Companies Specific/Day09_LongestConsecutiveSequence.java
+++ Companies Specific/Day09_LongestConsecutiveSequence.java
@@ -1,7 +1,34 @@
 import java.util.*;
 class Day09_LongestConsecutiveSequence {
 
-    //  Using HashSet
+    // BruteForce Approach - Linear Search
+    public static int longestConsecutive(int[] nums) {
+        int longest = 0;
+        for(int num : nums) {
+            int current = num;
+            int count = 1;
+            while(search(nums, current+1)) {
+                current++;
+                count++;
+            }
+            longest = Math.max(count, longest);
+        }
+        return longest;
+    }
+
+    public static boolean search(int[] nums, int target) {
+        for(int num: nums) {
+            if(num == target) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+
+
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)) {
             int N = sc.nextInt();
@@ -9,7 +36,7 @@ class Day09_LongestConsecutiveSequence {
             for(int i=0; i<N; i++) {
                 nums[i] = sc.nextInt();
             }
-            System.out.println(Arrays.toString(nums));
+            System.out.println(longestConsecutive(nums));
         }
     }
 }
