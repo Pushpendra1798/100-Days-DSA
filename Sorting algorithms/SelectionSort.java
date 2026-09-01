ce325f376fea66a06a059365ca5db1d0bf439e5b
Pushpendra Shah <pushpendra7725@gmail.com>
2026-09-01 15:52:41 +0530

    Time Complexity: O(n) |  Not Stable

---
+++ Sorting algorithms/SelectionSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+
+        for(int i=0; i<N; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        for(int i=0; i<N-1; i++) {
+            int smallestIdx = i;
+            for(int j=i+1; j<N; j++) {
+                if(nums[j] < nums[smallestIdx]){
+                    smallestIdx = j;
+                }
+            }
+
+            int temp = nums[i];
+            nums[i] = nums[smallestIdx];
+            nums[smallestIdx] = temp;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
