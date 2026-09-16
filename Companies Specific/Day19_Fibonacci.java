3b8f0f6346656ff43f414e7b4d7644670b039753
Pushpendra Shah <pushpendra7725@gmail.com>
2026-09-17 03:23:32 +0530

    using tabulation bottom up approach

---
+++ Companies Specific/Day19_Fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Day19_Fibonacci {
+
+    public static int fibo(int n) {
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of n: ");
+        int n = sc.nextInt();
+        System.out.print(fibo(n));
+    }
+}
