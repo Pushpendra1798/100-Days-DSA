97f3a91eae8cc5e89f14aa1316b0ab7dcfdda700
Pushpendra Shah <pushpendra7725@gmail.com>
2026-09-16 12:31:45 +0530

    Using Memoization

---
+++ Companies Specific/Day18_FibonacciSeries.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Day18_FibonacciSeries {
+
+    public static int fibo(int n, int[] dp) {
+        // base case
+        if(n <= 1) return n;
+
+        if(dp[n] != -1) return dp[n];
+
+        dp[n] = fibo(n-1, dp) + fibo(n-2, dp);
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of n: ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("Error: n must be a non-negative number.");
+            return;
+        }
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        int result = fibo(n, dp);
+        System.out.println("Fibonacci: " + result);
+    }
+}
